3bb66c1554bcbc25cd43d1e052718a2952d01dd8
Marlon Braga <rdg.brg@gmail.com>
2018-11-13 16:50:32 -0500

    fucntions-reportingForDuty

---
+++ functions-reportingForDuty.js
@@ -0,0 +1,13 @@
+/*
+  DESCRIPTION / OBJECTIVE
+
+  Write a function, reportingForDuty(), that has two string parameters,
+  rank and lastName, and returns a string in the following format:
+  'rank lastName reporting for duty!'
+*/
+
+const reportingForDuty = (rank, lastName) => {
+  return `${rank} ${lastName} reporting for duty!`
+}
+
+ console.log(reportingForDuty('Private', 'Fido')) // 'Private Fido reporting for duty!'
