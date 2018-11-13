34c1693f1e04487d116e661f8310b886592a456b
Marlon Braga <rdg.brg@gmail.com>
2018-11-13 03:12:28 -0500

    functions-sleep-debt-calculator(3)

--- functions-sleep-debt-calculator.js
+++ functions-sleep-debt-calculator.js
@@ -11,46 +11,53 @@ const getSleepHours = day => {
   switch(day) {
     case 'monday':
       return 5.5;
+      break;
     case 'tuesday':
       return 5.5;
+      break;
     case 'wednesday':
-      return 6;
+      return 5;
+      break;
     case 'thursday':
       return 5.5;
+      break;
     case 'friday':
       return 5.5;
+      break;
     case 'saturday':
-      return 7;
+      return 10;
+      break;
     case 'sunday':
       return 12;
+      break;
+    default:
+      return 'error!'
   }
 };
-
+// Invoking Helper function in getActualSleepHours()
 const getActualSleepHours = () =>
-// Invoking Helper function in getActualSleepHours(), adding, and implicitly returning total
 getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
 
 const getIdealSleepHours = (idealHours) => {
   return idealHours * 7;
 };
-
 // Checking validity between hours slept and ideal hours slept. Returning values.
 const calculateSleepDebt = () => {
 	let actualSleepHours = getActualSleepHours();
 	let idealSleepHours = getIdealSleepHours(7);
 
-  let calculatedHours = idealSleepHours - actualSleepHours
-
   if (actualSleepHours === idealSleepHours) {
     return `You got the perfect amount of sleep! Total hours slept last week: ${actualSleepHours} hours.`
   }
   else if (actualSleepHours > idealSleepHours) {
-    return `You got more sleep than you needed. This is how much you're ove your ideal plan: ${-calculatedHours} hours more slept. Total hours slept last week: ${actualSleepHours} hours.`
+    return `You've got ${actualSleepHours - idealSleepHours} hour(s) of sleep more than you should have last week.`
+  }
+  else if (actualSleepHours < idealSleepHours) {
+    return `You should get some rest. You sleep ${idealSleepHours - actualSleepHours} hour(s) less than you should have last week. Total hours slept last week: ${actualSleepHours} hours.`
   }
   else {
-    return `You should get some rest. You owe to yourself ${calculatedHours} hours of sleep. Total hours slept last week: ${actualSleepHours} hours.`
+    return `Error! Something went wrong. Check your code.`
   }
 };
-
 // Invoking function and starting the program
 console.log(calculateSleepDebt());
