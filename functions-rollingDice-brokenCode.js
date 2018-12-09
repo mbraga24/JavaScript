6614cb0ac6975ae9a9f926c44a8f72f3cc375971
Marlon Braga <rdg.brg@gmail.com>
2018-12-08 23:49:03 -0500

    functions-rollingDice-brokenCode

---
+++ functions-rollingDice-brokenCode.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION / OBJECTIVE
+
+The boss asks:
+- "We wrote a function, rollTheDice(), which is supposed to simulate
+two dice being rolled and totalled. It's close to doing what we want,
+but there's something not quite right. Can you fix our code, please?"
+
+Marlon answers:
+- "I got you, boss!"
+*/
+
+const rollTheDice = () => {
+  // Math.random() gives us a random number from 0 up to, but not including, 1
+  // We multiplied that by 6 to get a number between 0 and up to, but not including, 6
+  // But since we actually wanted numbers from 1 to 6, inclusive, we added 1
+    let die1 = Math.floor(Math.random() * 6 + 1)
+    let die2 = Math.floor(Math.random() * 6 + 1)
+    diceSum = die1 + die2
+	  return diceSum
+}
+
+console.log(rollTheDice());
