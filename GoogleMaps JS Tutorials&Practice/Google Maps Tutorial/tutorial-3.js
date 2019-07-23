ce93bdaad1efe19e700c143f31958de7431bda9b
Marlon Braga <rdg.brg@gmail.com>
2019-07-23 15:11:50 -0400

    Google Maps tutorial-3

--- GoogleMaps JS Tutorials&Practice/Google Maps Tutorial/tutorial-3.js
+++ GoogleMaps JS Tutorials&Practice/Google Maps Tutorial/tutorial-3.js
@@ -1,6 +1,17 @@
+const elementMap = document.getElementById("map");
+
 function initMap() {
-  let location = {
-    lat: 40.979671,
-    lng: -74.119179
+
+  const location = {lat: 40.712776, lng: -74.005974};
+
+  let mapOptions = {
+    zoom: 11,
+    center: location 
   }
+
+  let map = new google.maps.Map(elementMap, mapOptions)
+  let marker = new google.maps.Marker({
+    position: location,
+    map: map
+  })
 }
