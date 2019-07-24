dc378b43fccda67df55355e2d3d25d153fc16015
Marlon Braga <rdg.brg@gmail.com>
2019-07-23 21:32:15 -0400

    Google Maps Practice

---
+++ GoogleMaps JS Tutorials&Practice/Google Maps Practice/main.js
@@ -0,0 +1,34 @@
+let mapElement = document.getElementById("map");
+
+function initMap() {
+  let mapOptions = {
+    center: {lat: 000000, lng: 000000}
+    // disableDefaultUI: true
+    // draggable: false
+    // mapTypeId: 'hybrid' // 'satellite' // 'roadmap'
+  }
+  let map = new google.maps.Map(mapElement, mapOptions);
+  let infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(event) {
+      let position = {
+        lat: event.coords.latitude,
+        lng: event.coords.longitude
+      };
+      infoWindow.setPosition(position);
+      infoWindow.setContent('Your location!');
+      infoWindow.open(map);
+    }, function() {
+      handleLocationError('Geolocation service failed', map.center());  
+    })
+  } else {
+    handleLocationError('No geolocation available', map.center());
+  }
+}
+
+function handleLocationError(content, position) {
+  infoWindow.setPosition(position)
+  infoWindow.setContent(content)
+  infoWindow.open(map)
+}
