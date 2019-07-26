d0c8b4771add70362babdb5e1619ba0118754888
Marlon Braga <rdg.brg@gmail.com>
2019-07-25 22:12:16 -0400

    Practice Lessons - Lesson 2

--- GoogleMaps JS Tutorials&Practice/Google Maps Practice/Practice Lessons/01-Lesson/main.js
+++ GoogleMaps JS Tutorials&Practice/Google Maps Practice/Practice Lessons/01-Lesson/main.js
@@ -11,9 +11,13 @@ function initMap() {
   google.maps.event.addDomListener(window, 'load', initMap)
 }
 
+// This code provides the injection of the script tag.
 function loadTheMap() {
   let script = document.createElement('script'); 
   script.type = 'text/javascript';
-  script.src = "KEY_HERE";
+  // script.src = ADD URL HERE
   document.body.appendChild(script);
 }
+
+// Load the map once the webpage is fully loaded.
+window.onload = loadTheMap;
