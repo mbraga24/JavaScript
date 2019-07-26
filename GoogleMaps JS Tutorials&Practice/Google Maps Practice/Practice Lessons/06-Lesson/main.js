018cbcd0aba11b63237696d8530201d0e225084b
Marlon Braga <rdg.brg@gmail.com>
2019-07-26 00:25:39 -0400

    Fixes

--- GoogleMaps JS Tutorials&Practice/Google Maps Practice/Practice Lessons/06-Lesson/main.js
+++ GoogleMaps JS Tutorials&Practice/Google Maps Practice/Practice Lessons/06-Lesson/main.js
@@ -2,18 +2,18 @@ let map_1, map_2, map_3;
 
 function initMap() {
   let mapOptions_1 = {
-    zoom: 16,
-    center: {lat: 40.783058, lng: -73.971252}
+    zoom: 12,
+    center: new google.maps.LatLng(40.783058, -73.971252)
   };
 
   let mapOptions_2 = {
-    zoom: 10,
-    center: {lat: 48.856613, lng: 2.352222}
+    zoom: 8,
+    center: new google.maps.LatLng(48.856613, 2.352222)
   }
 
   let mapOptions_3 = {
     zoom: 2,
-    center: {lat: -22.906847, lng: -43.172897}
+    center: new google.maps.LatLng(-22.906847, -43.172897)
   }
 
   map_1 = new google.maps.Map(document.getElementById("map_canvas_1"), mapOptions_1)
