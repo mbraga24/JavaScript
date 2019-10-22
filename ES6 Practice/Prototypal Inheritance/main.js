53cec9bd33d4ebcd632cb2a70e86e7739b861cc3
Marlon Braga <rdg.brg@gmail.com>
2019-10-21 20:05:33 -0400

    Children classes of main Bird class

--- ES6 Practice/Prototypal Inheritance/main.js
+++ ES6 Practice/Prototypal Inheritance/main.js
@@ -1,6 +1,6 @@
 // Define class Bird
-function Birds(type, color) {
-  // Define properties
+function Bird(type, color) {
+  // Define properties.
   this.type = type;
   this.color = color;
   this.eggs = 0;
@@ -19,5 +19,35 @@ function Birds(type, color) {
   this.lay_egg = function() {
     this.eggs++;
     console.log(`${this.color} ${this.type} laid an egg!`);
+  }
+}
+
+function Parrot(type, color) {
+  // Must call parent constructor and pass this to it.
+  Bird.call(this, type, color);
+
+  // Define method talk()
+  this.talk = function() {
+    console.log(`${this.color} ${this.type} is talking.`);
+  }
+}
+
+function Raven(type, color) {
+  // Must call parent constructor and pass this to it.
+  Bird.call(this, type, color);
+
+  // Define method solve_puzzle()
+  this.solve_puzzle = function() {
+    console.log(`${this.color} ${this.type} is solving a puzzle.`);
+  }
+}
+
+function Songbird(type, color) {
+  // Must call parent constructor and pass this to it.
+  Bird.call(this, type, color);
+
+  // Define method sing()
+  this.sing = function() {
+    console.log(`${this.color} ${this.type} is singing.`);
   }
 }
